8a625073147b0cffb18515da9d478f0c9f416ec4
KCDasanayaka <116500659+KCDasanayaka@users.noreply.github.com>
2024-04-05 00:02:27 +0530

    new channgers

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 // getAllCurrencies
 app.get("/getAllCurrencies", async (req, res) => {
-   const nameURL = "https://openexchangerates.org/api/currencies.json?app_id=3a1af94dd2d044a89c19b1acb4dd5b68";
+   const nameURL = "https://openexchangerates.org/api/currencies.json?app_id=ee8238a25b8c47c0be10812afe57d8dc";
  
    try {
      const nameResponse = await axios.get(nameURL, {
@@ -24,6 +24,30 @@ app.get("/getAllCurrencies", async (req, res) => {
    }
  });
  
+//get target amount 
+app.get("/convert", async (req, res) => {
+  const { date, sourceCurrency, targetCurrency, amountInSourceCurrency } = req.query;
+
+  try {
+    const dataUrl = `https://openexchangerates.org/api/historical/${date}.json?app_id=ee8238a25b8c47c0be10812afe57d8dc`;
+    const dataResponse = await axios.get(dataUrl);
+    const rates = dataResponse.data.rates;
+
+    // Calculate target amount
+    const sourceRate = rates[sourceCurrency];
+    const targetRate = rates[targetCurrency];
+    const targetAmount = (targetRate / sourceRate) * amountInSourceCurrency;
+
+    return res.json(targetAmount.toFixed(2));
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+
+
 
 app.listen(5000, () => {
   console.log("SERVER STARTED");
